9f64beab85029617b02331e81e0c0c872e1d6085
shivam Yadav <ydvshivam2006@gmail.com>
2026-09-17 11:15:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Leap Year/leap-year.java
@@ -0,0 +1,17 @@
+class Solution {
+    static boolean checkYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        }
+
+        if (year % 100 == 0) {
+            return false;
+        }
+
+        if (year % 4 == 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
